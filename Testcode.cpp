d3e483f4d7f1e292f6c9b439250b0d75a9a45224
TranDucHuy1712052 <38009176+TranDucHuy1712052@users.noreply.github.com>
2018-04-05 22:04:32 +0700

    Add files via upload

---
+++ Testcode.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+struct student {
+	int stt;
+	char id[8], name[100];
+};
+
+int main()
+{
+	student a[100];
+	int count = 0;
+	FILE *f = fopen("17APCS1-Students.csv", "r");
+	FILE *g = fopen("anotherFile.csv", "w");
+
+	char tmp[100];
+	fgets(tmp, 99, f);
+	fprintf(g, "%s", tmp);
+	fgets(tmp, 99, f);
+	fprintf(g, "%s", tmp);
+
+	for (; count < sizeof(a) / sizeof(a[0]); count++) {
+		int got = fscanf(f, "%d,%[^,],%[^\n]\n", &a[count].stt, &a[count].id, &a[count].name);
+		if (got != 3) break;
+		fprintf(g, "%d,%s,%s\n", a[count].stt, a[count].id, a[count].name);
+		printf("%d,%s,%s\n", a[count].stt, a[count].id, a[count].name);
+	}
+
+	fclose(f);
+	fclose(g);
+	printf("Da xong !");
+	getchar();
+	return 0;
+}
